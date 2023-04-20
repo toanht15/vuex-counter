56e1ad9a32fcccaf249cc08e4975c8d77c629d67
Hoang The Toan <hoangthetoan@192.168.2.15>
2023-04-20 15:36:27 +0700

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,35 @@
 import { createStore } from 'vuex'
-
+import axios from "axios";
 export default createStore({
   state: {
+    counter: 20
   },
   getters: {
+    counterSquared(state) {
+      return state.counter * state.counter
+    }
   },
   mutations: {
+    decreaseCounter(state, randomNumber) {
+      state.counter -= randomNumber
+    },
+    increaseCounter(state, randomNumber) {
+      state.counter += randomNumber
+    }
   },
   actions: {
+    decreaseCounter( {commit}) {
+      axios('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=10&count=1').then(response =>{
+        console.log(response.data[0])
+        commit('decreaseCounter', response.data[0])
+      })
+    },
+    increaseCounter( {commit}) {
+      axios('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=10&count=1').then(response =>{
+        console.log(response.data[0])
+        commit('increaseCounter', response.data[0])
+      })
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="counter">
+      {{ $store.state.counter }}
+    </div>
+    <div class="counter-squared">
+      {{ $store.state.counter }}
+      <sup>2</sup> =
+      {{ $store.getters.counterSquared }}
+    </div>
+    <div class="buttons">
+      <button @click="$store.dispatch('decreaseCounter')">-</button>
+      <button @click="$store.dispatch('increaseCounter')">+</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
+
+
 </script>
+
+<style>
+  .counter {
+    font-size: 100px;
+  }
+
+  .buttons button {
+    font-size: 50px;
+    width: 120px;
+    margin: 0 10px;
+  }
+
+  .counter-squared {
+    font-size: 40px;
+    margin-bottom: 20px;
+  }
+</style>
